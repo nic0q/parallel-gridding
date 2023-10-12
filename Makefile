2208de751d6c5b8b462fd89a82e5b7323e0817d6
Nícolas Farfán <nicolas.farfan.ch@usach.cl>
2023-10-12 17:43:34 -0300

    [fix]: Corrections

--- Makefile
+++ Makefile
@@ -1,18 +1,16 @@
-FLAGS = -Ofast # optimize the code
-
 all: gridding
 
 gridding: gridding.cpp
-	u++ $(FLAGS) -o gridding gridding.cpp
+	u++ -o gridding gridding.cpp
 
 clean:
 	rm -f *.o *.exe gridding datosgrideadosr.raw datosgrideadosi.raw
 
 test_grid:
-	./gridding -i hltau_test.csv -o datosgrideados -d 1 -N 100 -c 3 -t 5
+	./gridding -i hltau_test.csv -o datosgrideados -d 0.003 -N 2048 -c 3 -t 5
 
 test_gridding:
-	./gridding -i hltau_completo_uv.csv -o datosgrideados -d 2 -N 500 -c 10 -t 10
+	./gridding -i hltau_completo_uv.csv -o datosgrideados -d 0.003 -N 2048 -c 10 -t 10
 
 test_slurm:
 	sbatch gridding.slurm
--- gridding.cpp
+++ gridding.cpp
@@ -8,7 +8,7 @@
 #include <string>
 #include <vector>
 using namespace std;
-const double speed_of_light = 2.998 * 1e+8;
+const double speed_of_light = 2.998 * 1e8;
 
 _Cormonitor FileReader {
  public:
@@ -17,7 +17,18 @@ _Cormonitor FileReader {
   }
 
   ~FileReader() { file.close(); }
-
+  void write_file(vector<double> vc, int size, string& file) {
+    FILE* outputFile = fopen(file.c_str(), "wb");
+    if (!outputFile) {
+      cerr << "Error al abrir el archivo: " << file << endl;
+    }
+    if (fwrite(vc.data(), sizeof(double), size, outputFile) == size) {
+      cout << "All elements were written successfully" << endl;
+    } else {
+      cout << "There was an error while writing the elements" << endl;
+    }
+    fclose(outputFile);
+  }
   vector<string> next() {
     resume();
     return vtr;
@@ -27,30 +38,26 @@ _Cormonitor FileReader {
 
   void clean_vtr() { vtr.clear(); }
 
-  int n_line;
-
   vector<string> vtr;
 
  private:
   ifstream file;
   int chunk;
+  int n_line = 0;
 
   void main() {
     string line;
     while (true) {
+      suspend();
       for (int i = 0; i < chunk; i++) {  // Reading X lines of chunk
         if (!getline(file, line)) {
           break;
         } else {
           vtr.push_back(line);
-          if (n_line % 10000 == 0) {
-            cout << "read line " << n_line << endl;
-          }
-          // cout << "read line " << n_line << endl;
+          cout << "read " << n_line << endl;
           n_line += 1;
         }
       }
-      suspend();
     }
   }
 };
@@ -58,16 +65,24 @@ _Cormonitor FileReader {
 _Task MyTask {
  public:
   MyTask(int id, int N, double deltaX, FileReader& reader)
-      : id(id), N(N), deltaX(deltaX), reader(reader) {}
+      : id(id), N(N), deltaX(deltaX), reader(reader) {
+    fr.resize(N * N, 2);
+    fi.resize(N * N, 0);
+    wt.resize(N * N, 0);
+  }
 
   int id;
+  int a = 3;
   int N;
   double deltaX;
+  vector<double> fr;
+  vector<double> fi;
+  vector<double> wt;
   FileReader& reader;
   vector<string> vc;
 
  private:
-  double deg_to_rad(double deg) { return deg * (M_PI / 180); }
+  double deg_to_rad(double deg) { return deg * M_PI / (180 * 3600); }
 
   vector<double> str_to_vec(string str) {
     stringstream ss(str);
@@ -81,29 +96,34 @@ _Task MyTask {
   }
 
   void main() {
+    cout << "Start(" << id << ")" << endl;
+    double uk, vk, vr, vi, wk, f, deltaU, deltaV, ik, jk;
     while (!reader.is_done()) {
       vc = reader.next();
       vector<double> vis;  // string vector
-      // cout << "Task(" << id << ")" << endl;
+      cout << "Task(" << id << ")" << endl;
       for (int i = 0; i < vc.size(); i++) {
         vis = str_to_vec(vc[i]);  // double vector
-        double uk, vk, vr, vi, w, f, deltaU, deltaV, ik, jk;
-        int spc;
         uk = vis[0];
         vk = vis[1];
         vr = vis[3];
         vi = vis[4];
-        w = vis[5];
+        wk = vis[5];
         f = vis[6];
-        spc = vis[7];
+
+        uk = uk * (f / speed_of_light);  // to wave longitude
+        vk = vk * (f / speed_of_light);  // to wave longitude
+
         deltaU = 1 / (N * deg_to_rad(deltaX));  // to radians
-        deltaU = deltaU * f / speed_of_light;   // to wave longitude
+
         deltaV = deltaU;  // asuming deltav is equals to deltau
         ik = round((uk / deltaU) + (N / 2));
         jk = round((vk / deltaV) + (N / 2));
-        // cout << "uk: " << uk << " vk: " << vk << " vr: " << vr << " vi: " <<
-        // vi
-        //      << " | ik(" << ik << ") jk(" << jk << ")" << endl;
+        cout << "uk: " << uk << " vk: " << vk << " vr: " << vr << " vi: " << vi
+             << " wk: " << wk << " | ik(" << ik << ") jk(" << jk << ")" << endl;
+        // fr[ik * N + jk] = fr[ik * N + jk] + (wk * vr);
+        // fi[ik * N + jk] = fi[ik * N + jk] + (wk * vi);
+        // wt[ik * N + jk] = wt[ik * N + jk] + wk;
       }
       reader.vtr.clear();
     }
@@ -152,13 +172,12 @@ void uMain::main() {
   cout << "Chunk Size: " << c << endl;
   cout << "Number of Tasks: " << t << endl;
 
-  float *fr, *fi, *wt;
-  fr = new float[N * N];
-  fi = new float[N * N];
-  wt = new float[N * N];
+  vector<double> fr;
+  vector<double> fi;
+  vector<double> wt;
 
   FileReader reader(input_file_name, c);  // Comonitor object creation
-  MyTask** tasks = new MyTask*[t];        // Array of tasks
+  MyTask* tasks[t];                       // Array of tasks
 
   for (int i = 0; i < t; i++) {
     tasks[i] = new MyTask(i, N, deltaX, reader);  // Allocation
@@ -168,8 +187,19 @@ void uMain::main() {
     delete tasks[i];
   }
 
-  delete[] tasks;
-  delete[] fr;
-  delete[] fi;
-  delete[] wt;
+  // fr = tasks[0]->fr;
+  // fi = tasks[0]->fi;
+  // wt = tasks[0]->wt;
+
+  // for (int i = 0; i < N * N; i++) {
+  //   if (wt[i] != 0) {
+  //     fr[i] = fr[i] / wt[i];
+  //     fi[i] = fi[i] / wt[i];
+  //   }
+  // }
+  for (int i = 0; i < t; i++) {
+    cout << tasks[i]->a << endl;
+  }
+  string r_file = output_file_name + "r.raw";
+  string i_file = output_file_name + "i.raw";
 }
